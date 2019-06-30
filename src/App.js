f3cb2e2a465dd29d0da7bc0cd5201ee4cd1e7ccb
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-06-30 16:13:14 -0400

    remove unused asset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import items from './data/items';
 import copy from 'copy-to-clipboard';
